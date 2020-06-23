e7ba18423789cba037d5130ee46b144d92721960
thomasf77 <thomas.freiman@stockly.ai>
2020-06-23 13:32:04 +0200

    lol

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use bench_thomas_vs_elrendio::Message;
-fn thomas(messages: &[Message]) -> Option<&Message> {
+/* fn thomas(messages: &[Message]) -> Option<&Message> {
     let search_from = messages
         .iter()
         .rposition(|message| match message {
@@ -17,6 +17,24 @@ fn thomas(messages: &[Message]) -> Option<&Message> {
             Message::Received { is_flagged: false } => true,
             _ => false,
         })
+} */
+
+// truly oscar method
+fn thomas(messages: &[Message]) -> Option<&Message> {
+    let mut unhandled_received_message = None;
+    messages
+        .iter()
+        .inspect(|&message| match message {
+            Message::Received { is_flagged: false } => {
+                unhandled_received_message = Some(message);
+            }
+            _ => (),
+        })
+        .rposition(|message| match message {
+            Message::Received { is_flagged: true } | Message::Sent { is_manual: true } => true,
+            _ => false,
+        });
+    unhandled_received_message
 }
 fn elrendio(messages: &[Message]) -> Option<&Message> {
     let mut unhandled_received_message = None;
@@ -38,18 +56,6 @@ fn criterion_benchmark(c: &mut Criterion) {
     let history_happy = bench_thomas_vs_elrendio::history_happy();
     let history_happy2 = bench_thomas_vs_elrendio::history_happy2();
     let history_happy3 = bench_thomas_vs_elrendio::history_happy3();
-    c.bench_function("elrendio function", |b| {
-        b.iter(|| elrendio(black_box(&history)))
-    });
-    c.bench_function("elrendio function happy", |b| {
-        b.iter(|| elrendio(black_box(&history_happy)))
-    });
-    c.bench_function("elrendio function happy 2", |b| {
-        b.iter(|| elrendio(black_box(&history_happy2)))
-    });
-    c.bench_function("elrendio function happy 3", |b| {
-        b.iter(|| elrendio(black_box(&history_happy3)))
-    });
     c.bench_function("thomas function", |b| {
         b.iter(|| thomas(black_box(&history)))
     });
@@ -61,6 +67,18 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("thomas function happy 3", |b| {
         b.iter(|| thomas(black_box(&history_happy3)))
+    });
+    c.bench_function("elrendio function", |b| {
+        b.iter(|| elrendio(black_box(&history)))
+    });
+    c.bench_function("elrendio function happy", |b| {
+        b.iter(|| elrendio(black_box(&history_happy)))
+    });
+    c.bench_function("elrendio function happy 2", |b| {
+        b.iter(|| elrendio(black_box(&history_happy2)))
+    });
+    c.bench_function("elrendio function happy 3", |b| {
+        b.iter(|| elrendio(black_box(&history_happy3)))
     });
 }
 
